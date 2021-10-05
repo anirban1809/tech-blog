98cb49e6427de6ccc1ded6789ba615d219195a54
Anirban Deb Singha <anirban12321@gmail.com>
2021-10-06 01:55:32 +0530

    feat: created articles page and 404 fallback page

--- components/fallbacks/404.tsx
+++ components/fallbacks/404.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFoundPage = () => {
     return (
-        <div className=" justify-center">
+        <div className="flex justify-around">
             <h1>Error 404</h1>
             The page you requested has not been found.
         </div>
