377616598c18ed91efaee828d5f3d1cb4f7e4c4f
Anirban Deb Singha <anirban12321@gmail.com>
2021-10-10 23:58:54 +0530

    feat(image-content): Added border on article image content

--- components/article/contents/image.tsx
+++ components/article/contents/image.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ArticleImage = (props:{imgSource:string,caption?:string}) => {
     return (
         <div>
-            <img className=" block m-auto" width="70%" src={props.imgSource} alt="" />
+            <img className=" border-2 rounded-md block m-auto" width="70%" src={props.imgSource} alt="" />
             <p className="text-gray-500 flex justify-around my-10">{props.caption}</p>
         </div>
     )
